829d8fcc43aed49293bfb2006500db98ed9c28c8
Dong Qian <dongqian0206@gmail.com>
2017-07-04 11:05:08 +0800

    Update computeCostMulti.m

--- Ex1_LinearRegression/computeCostMulti.m
+++ Ex1_LinearRegression/computeCostMulti.m
@@ -9,10 +9,6 @@ m = length(y); % number of training examples
 % You need to return the following variables correctly 
 J = 0;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta
-%               You should set J to the cost.
-
 Z = New_X * theta-y;
 J = Z' * Z / (2 * m);
 
