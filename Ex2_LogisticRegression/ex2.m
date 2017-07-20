bc4b96effc9ebba0b4f6b59183e301eeaf5daa8e
Dong Qian <dongqian0206@gmail.com>
2017-07-20 20:23:17 +0800

    Update ex2.m

--- Ex2_LogisticRegression/ex2.m
+++ Ex2_LogisticRegression/ex2.m
@@ -1,4 +1,4 @@
-%% Gussian Discriminant Analysis£¨Ð­·½²îÊÇÏàµÈµÄ£©
+%% Gussian Discriminant Analysisï¼ˆsame covariance matricesï¼‰
 
 %% Initialization
 
@@ -22,7 +22,7 @@ legend('y=1', 'y=0')
 
 %% Gussian models
 
-total_num1 = size(find(y==1),1);%y=1µÄ¸öÊý
+total_num1 = size(find(y==1),1);
 total_num2 = size(find(y==0),1);
 
 H1 = [X(y==1,1),X(y==1,2)];
@@ -37,13 +37,13 @@ plot(u1(:,1),u1(:,2),'r^', 'MarkerSize',8 ,'LineWidth',2)
 plot(u2(:,1),u2(:,2),'r^', 'MarkerSize',8 ,'LineWidth',2)
 
 
-sigma1 = eye(size(H1,2));  %ÁÐÊý
+sigma1 = eye(size(H1,2));
 sigma2 = eye(size(H2,2));
 
 u11=repmat(u1,size(H1,1),1);
 u22=repmat(u2,size(H2,1),1);
 
-%G1=exp(-0.5*((H1-u11)*inv(sigma1)*(H1-u11)'))/((2*pi)*sqrt(det(sigma1)));%Á½Î¬d/2=1
+%G1=exp(-0.5*((H1-u11)*inv(sigma1)*(H1-u11)'))/((2*pi)*sqrt(det(sigma1)));
 G2 = exp(-0.5*((H2-u22)*inv(sigma1)*(H2-u22)'))/((2*pi)*sqrt(det(sigma2)));
 
 G1 = exp(-0.5*((H1-u11)*inv(sigma1)*(H1-u11)'))/((2*pi)*sqrt(det(sigma1)));
