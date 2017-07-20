74d5e12bbba7f5311561022cc9702afbc2fb1576
Dong Qian <dongqian0206@gmail.com>
2017-07-20 20:24:03 +0800

    Update oneVsAll.m

--- Ex3_Multi-classClassification_NeuralNetworks/oneVsAll.m
+++ Ex3_Multi-classClassification_NeuralNetworks/oneVsAll.m
@@ -2,7 +2,7 @@ function [Thetas]=oneVsAll(X,y,labels,lambda)
 
 [m, n] = size(X);
 X = [ones(m,1) X];
-initial_theta = zeros(n+1,1);
+initial_theta = zeros(n+1, 1);
 options = optimset('GradObj','on','MaxIter',50);
 for i = 1:labels
     [Thetas(i,:)] = fmincg(@(t)(lrCostFunction(t, X, (y==i), lambda)), initial_theta, options);
